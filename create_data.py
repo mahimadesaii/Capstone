2f76729fb00e67de785a69243bcea6732a0817e7
Mahima Desai <130761963+mahimadesaii@users.noreply.github.com>
2026-04-10 23:58:47 +0530

    Delete create_data.py

--- create_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('preprocessed_data_clean.csv')
-# Convert to JS variable
-with open('data.js', 'w') as f:
-    f.write("const aqiData = " + df.to_json(orient='records') + ";")
